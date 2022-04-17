1df600aec6844a9b031ca007d7f35fec4798ed79
sb <shaurjyab@gmail.com>
2022-04-16 23:43:13 -0700

    james: update pin mapping

--- mini_stepper_test_1_jimbo.ino
+++ mini_stepper_test_1_jimbo.ino
@@ -2,31 +2,64 @@ struct stepper_pin_config {
     int dir;
     int step;
 
+    // long int step_time = 0;
+    bool step_state;
+
     stepper_pin_config(int dir_, int step_)
     {
       dir = dir_;
       step = step_;
     }
+
+    void init()
+    {
+      pinMode(dir, OUTPUT);  
+      pinMode(step, OUTPUT); 
+              digitalWrite(dir, HIGH);
+ 
+    }
+
+    void update(/*int steps_desired*/)
+    {
+        // divide microsecond counter by 2048, to get
+        // aproximately 2.048ms
+        long int _micros = micros();
+        if ((_micros >> 11) & 1)
+          step_state = !step_state;
+
+        if (1) {
+            if (step_state) {
+                digitalWrite(this->step, HIGH);
+            } else {
+                digitalWrite(this->step, LOW);
+
+            }
+        }
+    }
 };
 
-constexpr int num_motors = 16;
+constexpr float deg_per_step = 1.6;
+constexpr int num_motors = 15;
 stepper_pin_config motors[num_motors] = {
+
     { 2, 3 },  
     { 4, 5 },
     { 6, 7 },
     { 8, 9 },
     { 10, 11 },
-    { 30, 31 },
-    { 32, 33 },
-    { 34, 35 },
-    { 36, 37 },
-    { 38, 39 },
-    { 40, 41 },
-    { 42, 43 },
+
     { 44, 45 },
     { 46, 47 },
     { 48, 49 },
     { 50, 51 },
+
+    { 52, 53 },
+
+    { 22, 23 },
+    { 24, 25 },
+    { 26, 27 },
+    { 28, 29 },
+    { 30, 31 },
 };
 
 int num_steps = 450;
